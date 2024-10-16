bb614e8568b6d265392211318213c83f3ebc9824
vituhaa <145931387+vituhaa@users.noreply.github.com>
2024-10-16 17:23:32 +0300

    Update tests_for_archieving_1.ps1

--- tests_for_archieving_1.ps1
+++ tests_for_archieving_1.ps1
@@ -1,34 +1,54 @@
+# 1) Функция 1.
 function generate_test_files {
     param (
-        [string]$path,
-        [int]$fileCount,
-        [int]$fileSizeMB
+        [string]$path, # путь, где будут созданы файлы
+        [int]$fileCount, # количество файлов для генерации 
+        [int]$fileSizeMB # размер каждого файла в Мб
     )
 
-    if (-not $path) {
+    if (-not $path) {    # если путь не указан 
         Write-Host "The path for generating files cannot be empty."
         exit 1
     }
 
-    if (-not (Test-Path -Path $path)) {
+    if (-not (Test-Path -Path $path)) {   
+    # Test-Path - команда проверки существования директории.  Test-Path -Path $path  - команда проверяет, существует ли файл по пути $path. 
+    # True - возвращает, если элемент существует.
         New-Item -Path $path -ItemType Directory | Out-Null
+        # New-Item - команда для создания новых файлов, директорий или других объектов.
+        # -Path $path - указывает путь, по которому нужно создать новый объект. (-Path - параметр, который указывавеи путь к файлу, директории или другому объекту).
+        # -ItemType Directory - указыавает тип создаваемого объекта - в данном случае, директорию (папку).
+        # Out-Null - команда для подавления вывода. она указывает повершеллу не выводить информацию о результатах команды.
     }
 
     Write-Host " Generating $fileCount files to $fileSizeMB MB in directory $path..."
-    for ($i = 1; $i -le $fileCount; $i++) {
+    for ($i = 1; $i -le $fileCount; $i++) { 
         $fileName = Join-Path $path "test_file_$i.dat"
         fsutil file createnew $fileName ($fileSizeMB * 1MB) | Out-Null
     }
+    # -le - less or equal.
+    # Join-Path - команда объединяет путь к директории $path и имя файла "test_file_$i.dat" в единый путь.
+    # Файл с расширением .dat — это общий формат данных, который может использоваться для хранения информации в различных форматах, в зависимости от приложения, 
+    # которое его создало. Само по себе расширение .dat не указывает на какой-то определённый тип данных, и такой файл может содержать как текстовую информацию,
+    # так и двоичные данные.
+    # fsutil file createnew - это утилита, которая создаёт новый файл. Она принимает два аргумента:
+    # 1) имя файла
+    # 2) размер файла в байтах
+    # $fileName - переменная содержит полное имя файла, который будет создан, включая путь.
+    # $fileSizeMB - переменная, которая содержит размер файла в мб.
+    # умножение позволяет легко получить размер файла в байтах.
 }
 
+
+# 2) Функция 2.
 function run_test {
     param (
-        [string]$sourcePath,
-        [string]$archivePath,
-        [int]$diskUsageThreshold,
-        [int]$fileCount,
-        [int64]$maxSize,
-        [string]$testName
+        [string]$sourcePath, # строка указывающая путь к директории, содержащей файлы, которые будут архивированы.
+        [string]$archivePath, # стркоа, указывающая путь, куда будет сохранён архив
+        [int]$diskUsageThreshold, # порог использования в процентах
+        [int]$fileCount, # кол-во файлов, котооые будет обработаны в тесте.
+        [int64]$maxSize, # макс размер архива.
+        [string]$testName # строка, представляющая имя теста.
     )
 
     if (-not $sourcePath) {
@@ -41,27 +61,51 @@ function run_test {
         exit 1
     }
 
-    Write-Host "`nTest: $testName"
+    Write-Host "`nTest: $testName" # `n - переход на новую строку.
     Write-Host "------------------------------------"
     
     & .\test_0.ps1 -sourcePath $sourcePath -archivePath $archivePath -diskUsageThreshold $diskUsageThreshold -fileCount $fileCount -maxSize $maxSize
-
+    # & - оператор вызова.
+    # В PowerShell, если вы просто напишете путь к скрипту, система попытается интерпретировать это как текст или строку, а не как команду.
+    # .\test_0.ps1 - запуск как скрипт.
+    # запрос 5 параметров.
     $archiveName = Join-Path $archivePath "archive.zip"
-    if (Test-Path $archiveName) {
+    # переменная $archiveName созда1тся для хранения полного пути к файлу архива, который будет называться "archive.zip"
+    # Join-Path — это команда PowerShell, которая объединяет компоненты пути в файловой системе, делая это корректно с учётом разделителей пути
+    # (например, слэши \ или /).
+    if (Test-Path $archiveName) { # Test-Path - команда проверки существования директории.
         Write-Host "The $testName test was successful: The $archiveName archive was created."
     } else {
         Write-Host "The $testName test failed: The $archiveName archive was not created."
     }
-
     Write-Host "------------------------------------"
 }
 
+
+
+# 3) Создание тестовых файлов.
 $logDir = "$HOME/log"
 $backupDir = "$HOME/backup"
-
 generate_test_files -path $logDir -fileCount 20 -fileSizeMB 25
 
+
+
+# 4) Запуск тестов.
 run_test -sourcePath $logDir -archivePath $backupDir -diskUsageThreshold 50 -fileCount 10 -maxSize 1GB -testName "Test1"
 run_test -sourcePath $logDir -archivePath $backupDir -diskUsageThreshold 30 -fileCount 15 -maxSize 1GB -testName "Test2"
 run_test -sourcePath $logDir -archivePath $backupDir -diskUsageThreshold 80 -fileCount 10 -maxSize 1GB -testName "Test3"
 run_test -sourcePath $logDir -archivePath $backupDir -diskUsageThreshold 50 -fileCount 20 -maxSize 2GB -testName "Test4"
+
+
+
+# Краткое описание:
+# 1) Функция 1: эта функция генерирует тестовые файлы в указанной директории.
+# 2) Функция 2: эта функция запускает тест, который выполняет архивирование файлов из указанной директории на основе заданных параметров.
+# 3) Создание тестовых файлов.
+# 4) Запуск тестов.
+
+
+
+
+
+
